8588c4085e3adc96accf6403e751db57ec58f0f4
Boscoh722 <boscobrilli8@gmail.com>
2025-11-16 08:29:52 +0300

    Updated env

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,7 +36,7 @@ app.use(
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || 'https://food-nett.vercel.app/',
     credentials: true,
   })
 );
